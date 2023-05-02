60a01b9b62eb7e3da39ddaaf2d251b2f1d5971bd
hak.dok0@gmail.com <hak.dok0@gmail.com>
2023-05-02 13:28:27 +0300

    Добавьте файлы проекта.

---
+++ Kosta.TestTask/Views/Shared/ScriptsPartical.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Kosta.TestTask.Views.Shared
+{
+    public class ScriptsParticalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
